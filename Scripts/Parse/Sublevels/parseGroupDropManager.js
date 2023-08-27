9df70df2ef0f665ec1f0f2ea44cb0ae399498290
Dodiou <dodiou.csc@gmail.com>
2023-08-27 01:33:56 -0400

    - fix groupDropManager parser   - add default drop   - ignore multi-drops. Game only drops the first item

--- Scripts/Parse/Sublevels/parseGroupDropManager.js
+++ Scripts/Parse/Sublevels/parseGroupDropManager.js
@@ -7,19 +7,42 @@ export function isGroupDropManagerComp(comp) {
   return !!comp.Properties.GroupDropManager;
 }
 
+/*
+ * Occurs in
+ * - Secluded Courtyard F00
+ * - Subterranean Swarm F01
+ * - Doppelganger's Den F01
+ * - Hectic Hollows F00
+ */
+const DEFAULT_DROP = {
+  item: 'GPiecePick_C',
+  chance: 1,
+  min: 1,
+  max: 1,
+}
+
 export function parseGroupDropManagerComp(comp, compsList) {
   const GroupDropManager = getObjectFromPath(comp.Properties.GroupDropManager, compsList);
 
   const ignoreIds = GroupDropManager.Properties.GroupDropManagerAIParameter.IgnoreCIDList;
   const radius = GroupDropManager.Properties.GroupDropManagerAIParameter.GroupingRadius;
-  // TODO: there are a few GorupDropManager's that don't drop anything. Do they have a default?
-  const drops = GroupDropManager.Properties.GroupDropManagerAIParameter?.DropParameter?.DropItemParameter
-    ? parseDropList(GroupDropManager.Properties.GroupDropManagerAIParameter.DropParameter)
-    : [];
-  if (drops.length !== 1) {
-    console.warn('GroupDropManager drops something other than a single item.');
+  let drops = GroupDropManager.Properties.GroupDropManagerAIParameter?.DropParameter?.DropItemParameter
+    ? parseDropList(GroupDropManager.Properties.GroupDropManagerAIParameter.DropParameter, DEFAULT_DROP)
+    : [DEFAULT_DROP];
+
+  if (drops.length > 1) {
+    /*
+     * Only first item is dropped. See:
+     * - Industrial Maze F02
+     * - Kingdom of Beasts F05
+     * - Cavern for a King F02, F04, F15
+     * - Doppelganger's Den F03
+     * - Dream Home
+     */
+    drops = drops.slice(0, 1);
   }
   if (!radius) {
+    // CfaK F02
     console.warn(`Unknown/bad GroupDropManager radius: ${radius}.`);
   }
 
